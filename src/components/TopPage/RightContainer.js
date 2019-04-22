dc52ab9c223da6b19d23035ab554e03582343ddf
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-22 21:25:07 +0900

    mainpage/rightcontainer

--- src/components/TopPage/RightContainer.js
+++ src/components/TopPage/RightContainer.js
@@ -3,13 +3,14 @@ import ContentsHeader from './ContentsHeader'
 import classes from '../../assets/mainPage/RightContainer.css'
 import TrendContainer from './TrendContainer'
 import RankingContainer from './RankingContainer'
+import ChartContainer from '../ManagementPage/ChartContainer'
+
 class RightContainer extends Component {
     render(){
         return(
             <div>
                 <ContentsHeader content={'記録'} />
-                <div className={classes.sample_box}>
-                </div>
+                    <ChartContainer height={300} />
                 <ContentsHeader content={'ランキング'} />
                     <RankingContainer />
                 <ContentsHeader content={'トレンド'} />
