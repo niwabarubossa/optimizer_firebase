61fc93dfc4999d3ecabd5fa6c8c0de45808c9c94
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-22 20:44:22 +0900

    mainPage paddng

--- src/components/TopPage/MainContainer.js
+++ src/components/TopPage/MainContainer.js
@@ -15,7 +15,7 @@ class MainContainer extends Component {
     }
     render(){
         return(
-            <div className="mainContainer">
+            <div className={classes.mainContainer}>
                 <Grid container spacing={16} >
                     <Grid item xs={12} md={7}>
                         <div className={classes.contentCardContainer}>
