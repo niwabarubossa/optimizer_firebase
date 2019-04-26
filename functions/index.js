f9ddaf762be79ff968afeafd5a74ccfd8df31e7a
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-26 10:39:23 +0900

    firebase cloud functions ok!

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
+// const functions = require('firebase-functions');
+
+// // // Create and Deploy Your First Cloud Functions
+// // // https://firebase.google.com/docs/functions/write-firebase-functions
+// //
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+      return res.redirect(303, snapshot.ref.toString());
+    });
+});
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
--- src/index.js
+++ src/index.js
@@ -15,23 +15,6 @@ const enhancer = process.env.NODE_ENV === 'development' ?
 composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 const store = createStore(reducer, enhancer)
 
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-      return res.redirect(303, snapshot.ref.toString());
-    });
-});
-
 ReactDOM.render(
     <MuiThemeProvider>
         <Provider store={store}>
