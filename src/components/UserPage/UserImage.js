7ef77030c0b5a82dbe090747a00b4d8262286b3a
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-04-26 19:47:32 +0900

    userpage resume

--- src/components/UserPage/UserImage.js
+++ src/components/UserPage/UserImage.js
@@ -5,6 +5,7 @@ class UserImage extends Component {
     render(){
         return(
             <div className={css.userImageContainer}>
+                <img className={css.testImage} src="https://www.homepage-tukurikata.com/image/lion.jpg"></img>
             </div>
         )
     }
