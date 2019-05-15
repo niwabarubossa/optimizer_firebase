c73a4ba449ed44a7fd1299e61c85c1dc4bf8bbdb
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-05-15 18:29:41 +0900

    resume

--- functions/index.js
+++ functions/index.js
@@ -20,4 +20,57 @@ exports.addMessage = functions.https.onCall((data, context) => {
     return aiueo
     //{ data: 'aiueo' }
 });
-  
+
+exports.followAddActionCountUpdate = functions.firestore.document('users/{userId}/follow/{followUserId}').onCreate((change, context) => {
+    const followedRef = admin.firestore().collection('users').doc(context.params.userId)
+    //フォローボタンを押した人の参照
+    const followerRef = admin.firestore().collection('users').doc(context.params.followUserId)
+    //フォローボタンを押された人の参照
+    return followedRef.runTransaction(async (t) => {
+
+        const follower = await t.get(followerRef);
+        //フォロー押した人の
+        const followed = await t.get(followedRef);
+        //フォローボタンを押された人の参照
+   
+        const followerUpdate = {
+          followingCount: (follower.data().followingCount || 0) + 1
+        };
+        const followedUpdate = {
+          followerCount: (followed.data().followerCount || 0) + 1
+        };
+   
+        await t.set(followerRef, followerUpdate, { merge: true });
+        await t.set(followedRef, followedUpdate, { merge: true });
+   
+        return t;
+      });
+
+});
+
+exports.followDeleteActionCountUpdate = functions.firestore.document('users/{userId}/follow/{followUserId}').onDelete((change, context) => {
+    const followedRef = admin.firestore().collection('users').doc(context.params.userId)
+    //フォローボタンを押した人の参照
+    const followerRef = admin.firestore().collection('users').doc(context.params.followUserId)
+    //フォローボタンを押された人の参照
+    return followedRef.runTransaction(async (t) => {
+
+        const follower = await t.get(followerRef);
+        //フォロー押した人の
+        const followed = await t.get(followedRef);
+        //フォローボタンを押された人の参照
+   
+        const followerUpdate = {
+          followingCount: (follower.data().followingCount || 0) - 1
+        };
+        const followedUpdate = {
+          followerCount: (followed.data().followerCount || 0) - 1
+        };
+   
+        await t.set(followerRef, followerUpdate, { merge: true });
+        await t.set(followedRef, followedUpdate, { merge: true });
+   
+        return t;
+      });
+
+});
