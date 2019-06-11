00194ada2bd0f2ac254480040ee0c5a2f57889c2
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-06-11 10:51:48 +0900

    graph data flow edit

--- src/components/ManagementPage/ChartContainer.js
+++ src/components/ManagementPage/ChartContainer.js
@@ -6,46 +6,60 @@ import {
     BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer
   } from 'recharts';
 
-const data = [
-    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400,},
-    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210,},
-    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290,},
-    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000,},
-    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181,},
-    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500,},
-    {name: 'Page G', uv: 390, pv: 4300, amt: 2100,},
-    {name: 'Page H', uv: 3550, pv: 4300, amt: 2100,},
-    {name: 'Page I', uv: 7790, pv: 4300, amt: 2100,},
-    {name: 'Page J', uv: 3490, pv: 4300, amt: 2100,},
-    {name: 'Page J', uv: 3490, pv: 4300, amt: 2100,},
-    {name: 'Page J', uv: 3490, pv: 4300, amt: 2100,},
-    {name: 'Page J', uv: 3490, pv: 4300, amt: 2100,},
-    {name: 'Page G', uv: 390, pv: 4300, amt: 2100,},
-    {name: 'Page G', uv: 390, pv: 4300, amt: 2100,},
-    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181,},
-  ];
+//   const data = [
+//     {name: 'Page A', uv: 234, pv: 12, amt: 2400,},
+//     {name: 'Page B', uv: 255, pv: 12, amt: 2210,},
+//     {name: 'Page C', uv: 255, pv:15, amt: 2290,},
+//     {name: 'Page D', uv: 278, pv: 18, amt: 2000,},
+//     {name: 'Page E', uv: 256, pv: 19, amt: 2181,},
+//     {name: 'Page F', uv: 290, pv: 19, amt: 2500,},
+//     {name: 'Page G', uv: 390, pv: 20, amt: 2100,},
+//     {name: 'Page H', uv: 400, pv: 24, amt: 2100,},
+//     {name: 'Page I', uv: 402, pv: 26, amt: 2100,},
+//     {name: 'Page J', uv: 403, pv: 27, amt: 2100,},
+//     {name: 'Page J', uv: 412, pv: 27, amt: 2100,},
+//     {name: 'Page J', uv: 400, pv: 28, amt: 2100,},
+//     {name: 'Page J', uv: 420, pv: 28, amt: 2100,},
+//     {name: 'Page G', uv: 445, pv: 28, amt: 2100,},
+//     {name: 'Page G', uv: 450, pv: 30, amt: 2100,},
+//     {name: 'Page E', uv: 455, pv: 31, amt: 2181,},
+// ];
 
 class ChartContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          snackbarOpen: false
+        }
+        // pv -> total_action_amount
+        // uv -? total_score_amount
+        this.data = [
+            {name: 'Page A', total_score_amount: 234, pv: 12},
+            {name: 'Page B', total_score_amount: 255, pv: 12},
+            {name: 'Page C', total_score_amount: 255, pv:15},
+            {name: 'Page D', total_score_amount: 278, pv: 18},
+            {name: 'Page E', total_score_amount: 256, pv: 19},
+            {name: 'Page F', total_score_amount: 290, pv: 19},
+            {name: 'Page G', total_score_amount: 390, pv: 20},
+            {name: 'Page H', total_score_amount: 400, pv: 24},
+            {name: 'Page I', total_score_amount: 402, pv: 26},
+            {name: 'Page J', total_score_amount: 403, pv: 27},
+            {name: 'Page J', total_score_amount: 412, pv: 27},
+            {name: 'Page J', total_score_amount: 400, pv: 28},
+            {name: 'Page J', total_score_amount: 420, pv: 28},
+            {name: 'Page G', total_score_amount: 445, pv: 28},
+            {name: 'Page G', total_score_amount: 450, pv: 30},
+            {name: 'Page E', total_score_amount: 455, pv: 31},
+        ];
+      }
+
     render(){
         return(
             <div className={classes.chartContainer}>
                 <div className={classes.barChartContainer}>
-                  {/* <ResponsiveContainer width="100%" height={this.props.height} style={{zIndex: 1}} >
-                    <BarChart
-                        // margin={{ top: 10, right: 60, left: 20, bottom: 0 }}
-                        data={data}
-                        isBelow
-                        >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="pv" stackId="a" fill="#00b7ce" />
-                        <Bar dataKey="uv" stackId="a" fill="#beebad" />
-                    </BarChart>
-                  </ResponsiveContainer> */}
-                  <ComposedChartContainer height={500} />
+
+                  <ComposedChartContainer height={500} new_data={this.data} />
 
                 </div>
             </div>
--- src/components/ManagementPage/ComposedChartContainer.js
+++ src/components/ManagementPage/ComposedChartContainer.js
@@ -1,60 +1,41 @@
 import React, { Component } from 'react';
 import classes from '../../assets/managementPage/ChartContainer.css'
-
 import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer,ComposedChart,Area,Line
+    Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer,ComposedChart,Area
   } from 'recharts';
 
-const data = [
-    {name: 'Page A', uv: 234, pv: 12, amt: 2400,},
-    {name: 'Page B', uv: 255, pv: 12, amt: 2210,},
-    {name: 'Page C', uv: 255, pv:15, amt: 2290,},
-    {name: 'Page D', uv: 278, pv: 18, amt: 2000,},
-    {name: 'Page E', uv: 256, pv: 19, amt: 2181,},
-    {name: 'Page F', uv: 290, pv: 19, amt: 2500,},
-    {name: 'Page G', uv: 390, pv: 20, amt: 2100,},
-    {name: 'Page H', uv: 400, pv: 24, amt: 2100,},
-    {name: 'Page I', uv: 402, pv: 26, amt: 2100,},
-    {name: 'Page J', uv: 403, pv: 27, amt: 2100,},
-    {name: 'Page J', uv: 412, pv: 27, amt: 2100,},
-    {name: 'Page J', uv: 400, pv: 28, amt: 2100,},
-    {name: 'Page J', uv: 420, pv: 28, amt: 2100,},
-    {name: 'Page G', uv: 445, pv: 28, amt: 2100,},
-    {name: 'Page G', uv: 450, pv: 30, amt: 2100,},
-    {name: 'Page E', uv: 455, pv: 31, amt: 2181,},
-  ];
-
 class ComposedChartContainer extends Component {
+    constructor(props) {
+      super(props);
+    }
+
     render(){
         return(
             <div className={classes.chartContainer}>
                 <div className={classes.barChartContainer}>
+
                   <ResponsiveContainer width="100%" height={this.props.height} style={{zIndex: 1}} >
-                    {/* <ComposedChart width="100%" height={250} data={data}> */}
-                    <ComposedChart height={250} data={data}>
+                    <ComposedChart height={250} data={this.props.new_data}>
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
                     <CartesianGrid stroke="#f5f5f5" />
-                    {/* <Area type="monotone" dataKey="amt" fill="#d7f0f6" stroke="#78e8ff" /> */}
                     <Bar dataKey="pv" barSize={20} fill="#1fa8d8" />
-                    {/* <Area type="monotone" dataKey="uv" fill="#e4f8de" stroke="#32cd32" /> */}
                     </ComposedChart>
                   </ResponsiveContainer>
+
                   <ResponsiveContainer width="100%" height={this.props.height} style={{zIndex: 1}} >
-                    {/* <ComposedChart width="100%" height={250} data={data}> */}
-                    <ComposedChart height={250} data={data}>
+                    <ComposedChart height={250} data={this.props.new_data}>
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
                     <CartesianGrid stroke="#f5f5f5" />
-                    {/* <Area type="monotone" dataKey="amt" fill="#d7f0f6" stroke="#78e8ff" /> */}
-                    {/* <Bar dataKey="pv" barSize={20} fill="#413ea0" /> */}
-                    <Area type="monotone" dataKey="uv" fill="#63c2de" stroke="#63c2de" />
+                    <Area type="monotone" dataKey="total_score_amount" fill="#63c2de" stroke="#63c2de" />
                     </ComposedChart>
                   </ResponsiveContainer>
+
                 </div>
             </div>
         )
