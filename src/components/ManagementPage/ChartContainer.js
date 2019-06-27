d40c9f2d26e6cfa86b8d1d69997dd094dab09a21
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-06-28 08:29:43 +0900

    resume

--- src/components/ManagementPage/ChartContainer.js
+++ src/components/ManagementPage/ChartContainer.js
@@ -75,92 +75,96 @@ class ChartContainer extends Component {
     //   async componentWillMount(){
     async componentDidMount(){
         await this.props.getPosts()
-        await this.props.getUserInformation()
+        // await this.props.getUserInformation()
+        if(this.props.current_user){
+            await this.props.getWeeklyPosts(this.props.current_user.uid)
+        }
     }
 
     async componentDidUpdate(prevProps) {
         　if (this.props.current_user !== prevProps.current_user) {
         　　await this.props.getWeeklyPosts(this.props.current_user.uid);
-        //累計スコアの算出
-        var all_score = 0;
-        for(i=0 ; i<this.props.tweets.length ; i++){
-            if(this.props.tweets[i].score){
-                all_score += this.props.tweets[i].score;
+            //累計スコアの算出
+            var all_score = 0;
+            for(i=0 ; i<this.props.tweets.length ; i++){
+                if(this.props.tweets[i].score){
+                    all_score += this.props.tweets[i].score;
+                }
             }
-        }
-        var total_minus =0;
-        var i,j;
-        var hairetu = [];
-        var milliseconds_array = []
-        var weekly_score_array = [0,0,0,0,0,0,0,0]
+            var total_minus =0;
+            var i,j;
+            var hairetu = [];
+            var milliseconds_array = []
+            var weekly_score_array = [0,0,0,0,0,0,0,0]
 
-        for(i=0;i<=7;i++){
-            var today = new Date()
-            var i_days_before_miliseconds = new Date().setDate(new Date().getDate() - i);
-            var i_day = new Date(i_days_before_miliseconds)
-            milliseconds_array.unshift(i_day.setHours(0,0,0,0))
-        }
-        for(i=0 ; i<=(this.props.weekly_posts.length-1) ;i++){
-            if((milliseconds_array[0] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[1])){
-                weekly_score_array[0] += this.props.tweets[i].score
+            for(i=0;i<=7;i++){
+                var today = new Date()
+                var i_days_before_miliseconds = new Date().setDate(new Date().getDate() - i);
+                var i_day = new Date(i_days_before_miliseconds)
+                milliseconds_array.unshift(i_day.setHours(0,0,0,0))
             }
-            else if((milliseconds_array[1] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[2])){
-                weekly_score_array[1] += this.props.tweets[i].score
+            for(i=0 ; i<=(this.props.weekly_posts.length-1) ;i++){
+                if((milliseconds_array[0] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[1])){
+                    weekly_score_array[0] += this.props.tweets[i].score
+                }
+                else if((milliseconds_array[1] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[2])){
+                    weekly_score_array[1] += this.props.tweets[i].score
+                }
+                else if((milliseconds_array[2] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[3])){
+                    weekly_score_array[2] += this.props.tweets[i].score
+                }
+                else if((milliseconds_array[3] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[4])){
+                    weekly_score_array[3] += this.props.tweets[i].score
+                }
+                else if((milliseconds_array[4] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[5])){
+                    weekly_score_array[4] += this.props.tweets[i].score
+                }
+                else if((milliseconds_array[5] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[6])){
+                    weekly_score_array[5] += this.props.tweets[i].score
+                }
+                else if((milliseconds_array[6] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[7])){
+                    weekly_score_array[6] += this.props.tweets[i].score
+                }
+                else if( (milliseconds_array[7] <= this.props.tweets[i].created_at) ){
+                    weekly_score_array[7] += this.props.tweets[i].score
+                }
+                else{
+                }
             }
-            else if((milliseconds_array[2] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[3])){
-                weekly_score_array[2] += this.props.tweets[i].score
+            console.log('---------weekly score array')
+            console.log(weekly_score_array)
+            console.log('---------weekly score array')
+            for(i= (weekly_score_array.length-1) ; i>=0 ; i--){
+                    total_minus -= weekly_score_array[i]
+                    hairetu.unshift(total_minus)
             }
-            else if((milliseconds_array[3] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[4])){
-                weekly_score_array[3] += this.props.tweets[i].score
-            }
-            else if((milliseconds_array[4] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[5])){
-                weekly_score_array[4] += this.props.tweets[i].score
-            }
-            else if((milliseconds_array[5] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[6])){
-                weekly_score_array[5] += this.props.tweets[i].score
-            }
-            else if((milliseconds_array[6] <= this.props.tweets[i].created_at) && (this.props.tweets[i].created_at <= milliseconds_array[7])){
-                weekly_score_array[6] += this.props.tweets[i].score
-            }
-            else if( (milliseconds_array[7] <= this.props.tweets[i].created_at) ){
-                weekly_score_array[7] += this.props.tweets[i].score
-            }
-            else{
-            }
-        }
-        console.log('---------weekly score array')
-        console.log(weekly_score_array)
-        console.log('---------weekly score array')
-        for(i= (weekly_score_array.length-1) ; i>=0 ; i--){
-                total_minus -= weekly_score_array[i]
-                hairetu.unshift(total_minus)
-        }
 
-        var temp_hairetu = []
-        for(i=0; i< hairetu.length -1   ;i++){
-            var temp = {name: i,
-                        total_score_amount: (all_score+hairetu[i])
-                    }
-            temp_hairetu.push(temp)
+            var temp_hairetu = []
+            for(i=0; i< hairetu.length -1   ;i++){
+                var temp = {name: i,
+                            total_score_amount: (all_score+hairetu[i])
+                        }
+                temp_hairetu.push(temp)
+            }
+            var today = {
+                name: 7,
+                total_score_amount: all_score
+            }
+            temp_hairetu.push(today)
+            // {name: 'Page A', total_score_amount: 234, total_action_amount: 12},
+            this.setState({local_molded_data: temp_hairetu})
         }
-        var today = {
-            name: 7,
-            total_score_amount: all_score
-        }
-        temp_hairetu.push(today)
-        // {name: 'Page A', total_score_amount: 234, total_action_amount: 12},
-        this.setState({local_molded_data: temp_hairetu})
+        // if (this.props.tweets !== prevProps.tweets){
+            // this.setState({local_molded_data: temp_hairetu})
+        // }
 
-        　}
     }
 
     render(){
         return(
             <div className={classes.chartContainer}>
                 <div className={classes.barChartContainer}>
-
                   <ComposedChartContainer height={500} new_data={this.data} weekly_posts={this.props.weekly_posts} hairetu={this.hairetu} local_molded_data={this.state.local_molded_data} />
-
                 </div>
             </div>
         )
