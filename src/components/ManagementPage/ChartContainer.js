db6cc188304243032c615e910b3bdf3b0f9039b0
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-06-28 18:16:29 +0900

    get weekly posts ok

--- src/components/ManagementPage/ChartContainer.js
+++ src/components/ManagementPage/ChartContainer.js
@@ -8,81 +8,25 @@ import {
     BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer
   } from 'recharts';
 
-//   const data = [
-//     {name: 'Page A', uv: 234, pv: 12, amt: 2400,},
-//     {name: 'Page B', uv: 255, pv: 12, amt: 2210,},
-//     {name: 'Page C', uv: 255, pv:15, amt: 2290,},
-//     {name: 'Page D', uv: 278, pv: 18, amt: 2000,},
-//     {name: 'Page E', uv: 256, pv: 19, amt: 2181,},
-//     {name: 'Page F', uv: 290, pv: 19, amt: 2500,},
-//     {name: 'Page G', uv: 390, pv: 20, amt: 2100,},
-//     {name: 'Page H', uv: 400, pv: 24, amt: 2100,},
-//     {name: 'Page I', uv: 402, pv: 26, amt: 2100,},
-//     {name: 'Page J', uv: 403, pv: 27, amt: 2100,},
-//     {name: 'Page J', uv: 412, pv: 27, amt: 2100,},
-//     {name: 'Page J', uv: 400, pv: 28, amt: 2100,},
-//     {name: 'Page J', uv: 420, pv: 28, amt: 2100,},
-//     {name: 'Page G', uv: 445, pv: 28, amt: 2100,},
-//     {name: 'Page G', uv: 450, pv: 30, amt: 2100,},
-//     {name: 'Page E', uv: 455, pv: 31, amt: 2181,},
-// ];
-
 class ChartContainer extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             local_molded_data:null
-            //  [
-            //     {name: 'Page A', total_score_amount: 234, total_action_amount: 12},
-            //     {name: 'Page B', total_score_amount: 255, total_action_amount: 12},
-            //     {name: 'Page C', total_score_amount: 255, total_action_amount:15},
-            //     {name: 'Page D', total_score_amount: 278, total_action_amount: 18},
-            //     {name: 'Page E', total_score_amount: 256, total_action_amount: 19},
-            //     {name: 'Page F', total_score_amount: 290, total_action_amount: 19},
-            //     {name: 'Page G', total_score_amount: 390, total_action_amount: 20},
-            //     {name: 'Page H', total_score_amount: 400, total_action_amount: 24},
-            //     {name: 'Page I', total_score_amount: 402, total_action_amount: 26},
-            //     {name: 'Page J', total_score_amount: 403, total_action_amount: 27},
-            //     {name: 'Page J', total_score_amount: 412, total_action_amount: 27},
-            //     {name: 'Page J', total_score_amount: 400, total_action_amount: 28},
-            //     {name: 'Page J', total_score_amount: 420, total_action_amount: 28},
-            //     {name: 'Page G', total_score_amount: 445, total_action_amount: 28},
-            //     {name: 'Page G', total_score_amount: 450, total_action_amount: 30},
-            //     {name: 'Page E', total_score_amount: 455, total_action_amount: 31},
-            // ]
         }
-        // this.data = [
-        //     {name: 'Page A', total_score_amount: 234, total_action_amount: 12},
-        //     {name: 'Page B', total_score_amount: 255, total_action_amount: 12},
-        //     {name: 'Page C', total_score_amount: 255, total_action_amount:15},
-        //     {name: 'Page D', total_score_amount: 278, total_action_amount: 18},
-        //     {name: 'Page E', total_score_amount: 256, total_action_amount: 19},
-        //     {name: 'Page F', total_score_amount: 290, total_action_amount: 19},
-        //     {name: 'Page G', total_score_amount: 390, total_action_amount: 20},
-        //     {name: 'Page H', total_score_amount: 400, total_action_amount: 24},
-        //     {name: 'Page I', total_score_amount: 402, total_action_amount: 26},
-        //     {name: 'Page J', total_score_amount: 403, total_action_amount: 27},
-        //     {name: 'Page J', total_score_amount: 412, total_action_amount: 27},
-        //     {name: 'Page J', total_score_amount: 400, total_action_amount: 28},
-        //     {name: 'Page J', total_score_amount: 420, total_action_amount: 28},
-        //     {name: 'Page G', total_score_amount: 445, total_action_amount: 28},
-        //     {name: 'Page G', total_score_amount: 450, total_action_amount: 30},
-        //     {name: 'Page E', total_score_amount: 455, total_action_amount: 31},
-        // ];
       }
 
-    //   async componentWillMount(){
     async componentDidMount(){
         await this.props.getPosts()
-        // await this.props.getUserInformation()
-        if(this.props.current_user){
-            await this.props.getWeeklyPosts(this.props.current_user.uid)
-        }
+        // if(this.props.current_user){
+        //     await this.props.getWeeklyPosts(this.props.current_user.uid)
+        // }
     }
 
     async componentDidUpdate(prevProps) {
-        　if (this.props.current_user !== prevProps.current_user) {
+        // 　if ( !(this.props.current_user) || (this.state.local_molded_data) == null ) {
+        　if ( prevProps.current_user != (this.props.current_user) || (this.state.local_molded_data) == null ) {
         　　await this.props.getWeeklyPosts(this.props.current_user.uid);
             //累計スコアの算出
             var all_score = 0;
