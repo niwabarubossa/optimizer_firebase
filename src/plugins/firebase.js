b60181d8ef997d3201c1a3186155e2f72395ddc8
niwabarubossa <38070474+niwabarubossa@users.noreply.github.com>
2019-07-05 17:38:18 +0900

    Update firebase.js

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyBPhbtLJmPTit-B3JLrSTuc1Va_o6y88AI",
-    authDomain: "optimizer-60ecc.firebaseapp.com",
-    databaseURL: "https://optimizer-60ecc.firebaseio.com",
-    projectId: "optimizer-60ecc",
-    storageBucket: "optimizer-60ecc.appspot.com",
-    messagingSenderId: "398819056008"
   };
 const firebaseApp = firebase.initializeApp(config);
 export const firestore = firebaseApp.firestore();
